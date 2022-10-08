5b3a3bf3d534a9402f28566ee8136453e75fc0c6
Pawel Skrzypczynski <pawelskrzyp@outlook.com>
2022-10-08 19:08:00 +0100

    Add project files.

---
+++ InterfacesCalculator/Calculus.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesCalculator
+{
+    internal class Calculus : ICalculate, IResult
+    { 
+        public void Welcome()
+        {
+            Console.WriteLine($"Welcome to Smart Calculator. HURA !!!!");
+            GetNumber();
+
+            static void Add(decimal number1, decimal number2)
+            {
+                decimal addresult = number1 + number2;
+                Console.WriteLine($"Addition result: {addresult}.");
+            }
+            static void Substract(decimal number1, decimal number2)
+            {
+                decimal subresult = number1 - number2;
+                Console.WriteLine($"Substraction result: {subresult}.");
+            }
+            static void Multiplicate(decimal number1, decimal number2)
+            {
+                decimal mulresult = number1 * number2;
+                Console.WriteLine($"Multiplicate result: {mulresult}.");
+            }
+            static void Divide(decimal number1, decimal number2)
+            {
+                decimal divresult = number1 / number2;
+                Console.WriteLine($"Division result: {divresult}.");
+            }
+            static void Percentage(decimal number1, decimal number2)
+            {
+                decimal perresult = (number1 / number2) * 100;
+                Console.WriteLine($"Percent result: {perresult} %.");
+            }
+            static void GetNumber()
+            {
+                Console.WriteLine("Number 1: ");
+                decimal number1 = decimal.Parse(Console.ReadLine());
+                Console.WriteLine("Number 2: ");
+                decimal number2 = decimal.Parse(Console.ReadLine());
+                Add(number1, number2);
+                Substract(number1, number2);
+                Multiplicate(number1, number2);
+                Divide(number1, number2);
+                Percentage(number1, number2);
+            }
+        }
+        public void Result()
+        {
+            Console.WriteLine($"Smart Calculus End");
+        }
+    }
+}
---
+++ InterfacesCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using InterfacesCalculator;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        ICalculate calculate = new Calculus();
+        calculate.Welcome();
+        IResult result = new Calculus();
+        result.Result();
+    }
+}
